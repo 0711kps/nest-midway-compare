3692eaa8315bf8b836f5fabcf644ea810f04bd0e
pero <0711kps@gmail.com>
2024-02-19 22:09:47 +0800

    chore: Init projects in both frameworks

---
+++ midway-project/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1708351656738_4186',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
